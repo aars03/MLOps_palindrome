c6cce7a7524a683407a4f1a147f224da24826fe6
John Doe <shindeaarti00001@gmail.com>
2024-04-11 13:58:58 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function isPalindrome(str) {
     // Remove non-alphanumeric characters and convert to lowercase
     str = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
     
-    // Check if the string is equal to its reverse
+    
     return str === str.split('').reverse().join('');
 }
 
